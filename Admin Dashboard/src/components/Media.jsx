ad290ea0dc70b32f7ca22a063fdadd9237232119
abdalla-samir <abdallasamir860@gmail.com>
2026-03-08 23:39:55 +0200

    update new product section

--- Admin Dashboard/src/components/Media.jsx
+++ Admin Dashboard/src/components/Media.jsx
@@ -13,23 +13,43 @@ export default function Media({ fileHandler, setFileHandler }) {
 
     return (
         <>
-            <label className="border-2 border-dashed border-primary  w-full h-50 mt-4 flex justify-center items-center">
-                <div className="flex flex-col gap-2 justify-center items-center">
-                    <DownloadIcon width={50} />
-                    <p className="text-center text-title-small">
-                        Drop your image here or
-                        <span className="text-primary"> Browse </span> <br />
-                        <span className="text-text-light text-body-medium">
-                            Support: JPG, JPEG, PNG
-                        </span>
-                    </p>
+            {!fileHandler && (
+                <label
+                    className={`border-2 border-dashed border-primary  w-full h-50 mt-4 flex justify-center items-center cursor-pointer ${fileHandler ? "hidden" : ""}`}
+                >
+                    <div className="flex flex-col gap-2 justify-center items-center">
+                        <DownloadIcon width={50} />
+                        <p className="text-center text-title-small">
+                            Drop your image here or
+                            <span className="text-primary"> Browse </span>{" "}
+                            <br />
+                            <span className="text-text-light text-body-medium">
+                                Support: JPG, JPEG, PNG
+                            </span>
+                        </p>
+                    </div>
+                    <input
+                        type="file"
+                        className="hidden"
+                        onChange={handleFileChange}
+                    />
+                </label>
+            )}
+            {fileHandler && (
+                <div className="flex flex-col items-center">
+                    <img
+                        src={fileHandler}
+                        alt="preview"
+                        className="w-46 h-46 shadow rounded"
+                    />
+                    <button
+                        className="btn mt-5"
+                        onClick={() => setFileHandler("")}
+                    >
+                        Remove Image
+                    </button>
                 </div>
-                <input
-                    type="file"
-                    className="hidden"
-                    onChange={handleFileChange}
-                />
-            </label>
+            )}
         </>
     );
 }
